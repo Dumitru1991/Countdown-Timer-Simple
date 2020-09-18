f9d114c5d176da10cb2591080149677b07b83190
Dumitru1991 <todorandumitru046@gmail.com>
2020-09-18 11:55:45 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const daysEL = document.getElementById("days");
+const hoursEL = document.getElementById("hours");
+const minsEL = document.getElementById("mins");
+const secondsEL = document.getElementById("seconds");
+
+
+const newYears = "1 Jan 2021";
+
+function countdown(){
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearsDate- currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+    console.log(days, hours, minutes, seconds);
+
+    daysEL.innerHTML = days;
+    hoursEL.innerHTML = formatTime(hours);
+    minsEL.innerHTML = formatTime(minutes);
+    secondsEL.innerHTML = formatTime(seconds);
+
+}
+
+function formatTime(time){
+    return time < 10 ? (`0${time}`) : time;
+}
+
+
+countdown();
+
+setInterval(countdown, 1000);
